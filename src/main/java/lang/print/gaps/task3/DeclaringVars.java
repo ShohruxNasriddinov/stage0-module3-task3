059024074afe83a4c40de7b9be6cfb0d2aff568d
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-11-17 13:14:15 +0500

    commit3 task3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,10 +2,8 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
         int a , b, c;
-        a=b=c=10;
-        System.out.println(a+"\t"+b+"\t"+c);
+        a = b = c = 10;
         System.out.println(c);
     }
 }
